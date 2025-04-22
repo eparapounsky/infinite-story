5fde6abd0fefe182db3d9cdf878e5c5f33aac0be
Elena Parapounsky <eparapounsky@gmail.com>
2025-04-22 12:52:18 -0700

    added icons to buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import { IoMdArrowForward } from "react-icons/io";
+import { BiUndo } from "react-icons/bi";
+import { AiOutlineRedo } from "react-icons/ai";
 
 function App() {
   return (
@@ -12,9 +15,9 @@ function App() {
       <textarea placeholder="Type your next prompt here..."></textarea>
 
       <div className="buttons">
-        <button>Continue</button>
-        <button>Undo</button>
-        <button>Regenerate</button>
+        <button>Continue <IoMdArrowForward /> </button>
+        <button>Undo <BiUndo /> </button>
+        <button>Regenerate <AiOutlineRedo /> </button>
       </div>
 
       <footer>© 2025 Infinite Story Generator. Created by Vincent Le & Elena Parapounsky.</footer>
