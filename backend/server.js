a29b759692676be56ecfa7071dc18e216d87079a
Elena Parapounsky <eparapounsky@gmail.com>
2025-04-27 11:02:43 -0700

    import openai and create instance

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,3 +18,7 @@ app.listen(port, () => {
 // Citation for file structure
 // Date: 4/22/2025
 // Copied from: https://expressjs.com/en/starter/hello-world.html
+
+// Citation for adding OpenAI API
+// Date: 4/27/2025
+// Copied from: https://community.openai.com/t/import-error-with-openaiapi-in-node-js-project/549074
