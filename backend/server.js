a04d4eab2d12a8a2c49f9a0353f46084a7b7ffef
Elena Parapounsky <eparapounsky@gmail.com>
2025-04-27 12:36:08 -0700

    change from commonjs 'require' to es module 'import'

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 5000
-const OpenAI = require("openai");
-require('dotenv').config(); // load environment variables
+import OpenAI from 'openai'
+// load environment variables
+import dotenv from 'dotenv'
+dotenv.config()
 
+// initialize openai client
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
+// express routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const response = await client.responses.create({
+  model: 'gpt-4o',
+  instructions: 'You are a coding assistant that talks like a pirate',
+  input: 'Are semicolons optional in JavaScript?',
+});
+
+console.log(response.output_text);
+
+// start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App  listening on port ${port}`)
 })
 
 // Citation for file structure
@@ -23,3 +36,7 @@ app.listen(port, () => {
 // Citation for adding OpenAI API
 // Date: 4/27/2025
 // Copied from: https://community.openai.com/t/import-error-with-openaiapi-in-node-js-project/549074
+
+// Citation 
+// Date: 4/27/2025
+// Copied from: https://www.npmjs.com/package/openai#:~:text=openai/openai%27%3B-,Usage,-The%20full%20API
