d87b0fb7a68d85605549a08b60c512d496d2e4f1
Elena Parapounsky <eparapounsky@gmail.com>
2025-04-27 11:15:36 -0700

    add dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const OpenAI = require("openai");
+require('dotenv').config(); // load environment variables
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
